4cc3d53372e3fa17f4458e7b1242db551debf4aa
Carlos Costa <carllos.nc@gmail.com>
2019-08-03 21:29:24 -0300

    Adding support to coveralls

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFiles: ["./jest.setup.js"],
   verbose: true,
   collectCoverage: true,
+  coverageReporters: ["lcov", "text"],
   collectCoverageFrom: [
     "./src/helpers/*.{js,jsx}",
     "!**/node_modules/**",
