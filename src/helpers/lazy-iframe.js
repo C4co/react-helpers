f049b89e6164f59cfed8f94ba10f6b8c234abad0
Carlos Costa <carllos.nc@gmail.com>
2019-08-17 18:35:49 -0300

    Few updates

--- src/helpers/lazy-iframe.js
+++ src/helpers/lazy-iframe.js
@@ -10,25 +10,25 @@ export class LazyIframe extends Component {
     this.frame = React.createRef()
   }
 
-  componentDidMount() {
+  isInViewport(elem) {
+    var bounding = elem.getBoundingClientRect()
+    const offset = 200
+
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom - offset <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    )
+  }
+
+  reload(){
     const iframe = this.frame.current
 
-    function isInViewport(elem) {
-      var bounding = elem.getBoundingClientRect()
-      const offset = 200
-
-      return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom - offset <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <=
-          (window.innerWidth || document.documentElement.clientWidth)
-      )
-    }
-
     const action = () => {
-      if (isInViewport(iframe)) {
+      if (this.isInViewport(iframe)) {
         iframe.src = this.props.source
 
         window.removeEventListener("scroll", action)
@@ -40,6 +40,10 @@ export class LazyIframe extends Component {
     window.addEventListener("resize", action)
 
     action()
+  }
+
+  componentDidMount() {
+    this.reload()
   }
 
   render() {
--- src/helpers/lazy-image.js
+++ src/helpers/lazy-image.js
@@ -11,25 +11,25 @@ export class LazyImage extends Component {
     this.myRef = React.createRef()
   }
 
+  isInViewport(elem){
+    var bounding = elem.getBoundingClientRect()
+    const offset = 200
+
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom - offset <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    )
+  }
+
   reload(){
     const item = this.myRef.current
 
-    const isInViewport = elem => {
-      var bounding = elem.getBoundingClientRect()
-      const offset = 200
-
-      return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom - offset <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <=
-          (window.innerWidth || document.documentElement.clientWidth)
-      )
-    }
-
     const action = () => {
-      if (isInViewport(item)) {
+      if (this.isInViewport(item)) {
         item.src = this.props.source
         item.style.opacity = "1"
 
