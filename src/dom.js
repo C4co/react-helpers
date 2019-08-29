d335c15de3c244bcac1b0426cc79dc58a3c12f80
Carlos Costa <carllos.nc@gmail.com>
2019-08-29 00:17:07 -0300

    Fixing element in viewport

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,9 @@
 export const isInViewport = elem => {
-  var bounding = elem.getBoundingClientRect()
-  const offset = 200
+  const bounding = elem.getBoundingClientRect()
+  const offset = 300
 
   return (
     bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom - offset <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
+    bounding.bottom - offset <= (window.innerHeight || document.documentElement.clientHeight)
   )
 }
