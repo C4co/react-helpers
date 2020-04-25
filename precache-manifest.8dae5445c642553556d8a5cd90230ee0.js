f145dc0efbb6eebf0105fed52e4bdbcb1ad6a35b
Carlos Costa <carllos.nc@gmail.com>
2020-04-25 14:14:26 -0300

    Updates

---
+++ precache-manifest.8dae5445c642553556d8a5cd90230ee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e419248af12e84c8dcb08b6636cbbe3",
+    "url": "/react-helpers/index.html"
+  },
+  {
+    "revision": "295350eba887f2a124c3",
+    "url": "/react-helpers/static/js/2.d71b3775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-helpers/static/js/2.d71b3775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c8a0d6d111ca040b81",
+    "url": "/react-helpers/static/js/main.4a7c2c64.chunk.js"
+  },
+  {
+    "revision": "131f29b66b8af6333dd6",
+    "url": "/react-helpers/static/js/runtime-main.6e73e25a.js"
+  }
+]);
